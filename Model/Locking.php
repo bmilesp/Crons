edc3313526ecbaa8570d27524adc3e1facbcf21f
bmilesp <bmilesp@gmail.com>
2014-06-11 18:12:05 -0400

    init

---
+++ Model/Locking.php
@@ -0,0 +1,21 @@
+<?php 
+class Locking extends AppModel {
+	var $useDbConfig = 'crons_locking';
+	var $useTable = false;
+	var $actsAs = array('Endpoints.Endpoint');
+	
+	function lock($slug = null){
+		$result = $this->callEndpoint('/c2/crons/locks/lock/'.$slug);
+		if($result->body == '0'){
+			return false; //another lock is running
+		}else{
+			return true;
+		}
+	}
+
+	function unlock($slug = null){
+		$result = $this->callEndpoint('/c2/crons/locks/unlock/'.$slug);
+		return $result->body; 
+		
+	}
+}
